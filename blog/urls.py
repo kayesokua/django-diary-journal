5e329422b8713290ed30b61dc55666da90b7e7ba
蔡錦絲 <caijinsi@sainishikiitos-MacBook-Air.local>
2022-10-17 08:54:42 +0200

    updated path to repath on site.urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from . import views
-from django.urls import include
-from django.conf.urls import url
+
+from django.urls import re_path
+from django.urls import include, path
 from django.urls import path
 from .feeds import LatestPostsFeed, AtomSiteNewsFeed
 
@@ -9,7 +10,7 @@ urlpatterns = [
     path("feed/atom", AtomSiteNewsFeed()),
     path("", views.PostList.as_view(), name="home"),
     path("about", views.about_me, name="about_me"),
-    url(r'^tagged/(?P<tag>[\w-]+)/$', views.tag_detail, name='tag_detail'),
+    re_path(r'^tagged/(?P<tag>[\w-]+)/$', views.tag_detail, name='tag_detail'),
     path("<slug:slug>/", views.post_detail, name="post_detail"),
     path("<slug:slug>/like", views.like_post, name="like"),
     path("<slug:slug>/unlike", views.unlike_post, name="unlike"),
